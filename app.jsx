32fe15951f5f1f67f9460e75a4c977236d2fa74e
deonna <dnhodges@gmail.com>
2016-09-11 18:02:18 -0700

    feat(stopwatch): Adding tick functionality to Stopwatch and tearing down time interval when it's no longer visible to avoid memory leaks.

--- app.jsx
+++ app.jsx
@@ -82,13 +82,66 @@ Stats.propTypes = {
 };
 
 let Stopwatch = React.createClass({
+
+  getInitialState: function() {
+    return {
+      running: false,
+      elapsedTime: 0,
+      previousTime: 0,
+    };
+  },
+
+  componentDidMount: function() {
+    this.interval = setInterval(this.onTick, 1000);
+  },
+
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+
+  onTick: function() {
+    if (this.state.running) {
+      let now = Date.now();
+
+      this.setState({
+        previousTime: now,
+        elapsedTime: this.state.elapsedTime + (now - this.state.previousTime),
+      })
+    }
+  },
+
+  onStart: function() {
+    this.setState({
+      running: true,
+      previousTime: Date.now(),
+    });
+  },
+
+  onStop: function() {
+    this.setState({
+      running: false,
+    });
+  },
+
+  onReset: function() {
+    this.setState({
+      elapsedTime: 0,
+    })
+  },
+
   render: function() {
+    let seconds = Math.floor(this.state.elapsedTime / 1000);
+    console.log(seconds);
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
-        <div className="stopwatch-time">0</div>
-        <button>Start</button>
-        <button>Reset</button>
+        <div className="stopwatch-time">{seconds}</div>
+        {
+          this.state.running ?
+          <button onClick={this.onStop}>Stop</button> :
+          <button onClick={this.onStart}>Start</button>
+        }
+        <button onClick={this.onReset}>Reset</button>
       </div>
     );
   }
